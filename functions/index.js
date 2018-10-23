7cb68dfb2d86d4f6a3eb871896900c3e24562441
Vakme <4bielak.aleksandra@gmail.com>
2018-10-23 22:52:06 +0200

    hatching first egg

--- functions/index.js
+++ functions/index.js
@@ -5,15 +5,8 @@ admin.initializeApp();
 
 exports.createUserTable = functions.auth.user().onCreate(async (user) => {
   console.log('A new user signed in for the first time.');
-  const email = user.email || 'Anonymous';
-  
   // Saves the new welcome message into the database
   // which then displays it in the FriendlyChat clients.
-  await admin.database().ref('users').push({
-    email: email,
-    statistics: [],
-    monsters: [],
-    items: []
-  });
+  await admin.database().ref('users/' + user.uid + '/monsterCount').set(0);
   console.log('New user in the database.');
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    This is homepage
+      {{userData}}
+      <b-modal v-model="firstLogin">
+          Congratulations, you receive an egg!
+          And something just Hatched!
+          <div slot="modal-footer" class="w-100">
+              <b-btn @click="addMonster">Close Me</b-btn>
+          </div>
+      </b-modal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import db from '../db'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      userData: {},
+      firstLogin: false
+    }
+  },
+  computed: {
+    getUid() {
+      return this.$store.getters.getUser.uid;
+    }
+  },
+  firebase: function () {
+    let uid = this.getUid;
+    return {
+      userData: {
+        source: db.ref('users/' + uid),
+        asObject: true
+      }
+    }
+  },
+  watch: {
+    userData: function (user) {
+      if(user.monsterCount === 0) {
+        this.firstLogin = true;
+      }
+    }
+  },
+  methods: {
+    addMonster: function () {
+      this.$firebaseRefs.userData.child('monsterCount').set(1);
+      this.$firebaseRefs.userData.child('monsters').push({
+        name: 'Random',
+        type: 'Goldpuker'
+      });
+      this.firstLogin = false;
+    }
   }
 }
 </script>
