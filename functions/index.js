4743a4508ea8c8d3164639794dd8820155fbd843
Vakme <4bielak.aleksandra@gmail.com>
2018-10-23 23:07:14 +0200

    removed unused service workers

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,6 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.createUserTable = functions.auth.user().onCreate(async (user) => {
-  console.log('A new user signed in for the first time.');
-  // Saves the new welcome message into the database
-  // which then displays it in the FriendlyChat clients.
   await admin.database().ref('users/' + user.uid + '/monsterCount').set(0);
   console.log('New user in the database.');
 });
